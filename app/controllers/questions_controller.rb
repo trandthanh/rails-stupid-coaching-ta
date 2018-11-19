dea69f3b3d27c4dbdd70e7b7eec9b39a98112bbd
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-11-19 10:09:38 +0100

    Finished exercise

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:question] == "I am going to work!"
+      return @answer = "Great!"
+    elsif params[:question].include?("?")
+      return @answer = "Silly question, get dressed and go to work!"
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
